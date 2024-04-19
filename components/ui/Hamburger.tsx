3f46a4ba5ec089fc5b3a50231c81ee3ca39eb8da
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-04-19 14:02:35 +0500

    hamburger is all done but transition is not working

--- components/ui/Hamburger.tsx
+++ components/ui/Hamburger.tsx
@@ -26,9 +26,8 @@ function App() {
         onClick={toggleDrawer}
         className="absolute left-[88%] top-[5%]"
       />
-
       {isDrawerOpen && (
-        <div className="absolute z-10 h-[100vh] w-screen bg-[#F2EEE6]">
+        <div className="absolute z-10 h-[105vh] w-screen  translate-x-0 transform bg-[#F2EEE6] transition-transform duration-500 ease-in-out">
           <div className="flex items-center justify-between border-b px-4 py-4">
             <p className="text-dark-brown text-[28px] font-bold leading-[28px]">
               GROUNDS
@@ -44,47 +43,47 @@ function App() {
             <div className="flex flex-col gap-[35px]">
               <Link
                 href="/"
-                className=" text-darkBrown text-[26px] font-semibold leading-[28px]"
+                className="text-[26px] font-semibold leading-[28px] text-darkBrown"
               >
                 Home
               </Link>
               <Link
                 href="/"
-                className="text-darkBrown  text-[26px] font-semibold leading-[28px]"
+                className="text-[26px]  font-semibold leading-[28px] text-darkBrown"
               >
                 Coaching
               </Link>
               <Link
                 href="/"
-                className="text-darkBrown text-[26px] font-semibold leading-[28px]"
+                className="text-[26px] font-semibold leading-[28px] text-darkBrown"
               >
                 Prices
               </Link>
               <Link
                 href="/"
-                className="text-darkBrown  text-[26px] font-semibold leading-[28px]"
+                className="text-[26px]  font-semibold leading-[28px] text-darkBrown"
               >
                 Programmes{" "}
               </Link>
               <Link
                 href="/"
-                className="text-darkBrown text-[26px] font-semibold leading-[28px]"
+                className="text-[26px] font-semibold leading-[28px] text-darkBrown"
               >
                 Support
               </Link>
             </div>
             <div>
-              <Link href="/">
-                <p className="bg-darkBrown mt-[50px] flex h-[56px] items-center justify-center rounded-[24px] text-[16px] font-medium leading-[24px] text-white">
-                  Get Started
-                </p>
-                <p className="text-blackOpacity mt-[16px] text-center text-[14px] font-normal leading-[18px]">
-                  Already have an account?{" "}
-                  <span className="text-darkBrown underline ">
-                    <Link href="/">Log In</Link>
-                  </span>
-                </p>
-              </Link>
+              {/* <Link href="/"> */}
+              <p className="mt-[50px] flex h-[56px] items-center justify-center rounded-[24px] bg-darkBrown text-[16px] font-medium leading-[24px] text-white">
+                Get Started
+              </p>
+              <p className="mt-[16px] text-center text-[14px] font-normal leading-[18px] text-blackOpacity">
+                Already have an account?{" "}
+                <span className="text-darkBrown underline ">
+                  <Link href="/">Log In</Link>
+                </span>
+              </p>
+              {/* </Link> */}
             </div>
             <div className="border-caption my-0 mb-[32px] mt-[32px] w-full border-t-[0.5px] opacity-50"></div>
             <div className=" mt-[22px] flex items-center justify-center gap-[15px]">
