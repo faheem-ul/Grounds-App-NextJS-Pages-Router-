1a874413bf4ae018e2149ac921b5068aa41c34f9
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-04-23 11:34:03 +0500

    firebase setup is done

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB24_U9Hr45fjUe2vNQmUh8Z28qWb0NR5U",
+  authDomain: "grounds-app-9bba6.firebaseapp.com",
+  projectId: "grounds-app-9bba6",
+  storageBucket: "grounds-app-9bba6.appspot.com",
+  messagingSenderId: "240898864864",
+  appId: "1:240898864864:web:a11f3b1b19992426598285",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
