abc1cd4ef5cd7b22f8a9c3c93d9c423167f44f39
Faheem Ul Hassan Aziz <seebiz.gpt0623.dev56@gmail.com>
2024-04-23 23:16:56 +0500

    working on context

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+"client side";
 
-function AuthContext() {
-  return <div>AuthContext</div>;
+import React, { useEffect } from "react";
+import { useState } from "react";
+
+import { auth } from "../firebaseConfig";
+
+import PropTypes from "prop-types";
+import { User } from "firebase/auth";
+
+interface AuthContextType {
+  user: string;
 }
 
-export default AuthContext;
+export const AuthContext = React.createContext<AuthContextType>({
+  user: "",
+});
+
+function AuthProvider({ children }: any) {
+  const [user, setUser] = useState<string>("ok");
+
+  // setUser("ok");
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+}
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
